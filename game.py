c280342cdaed69850dabdaba48f858bfe50234cb
Marco Auer <marcoa@ThinkPadT14.fritz.box>
2026-01-09 09:37:36 +0100

    gitignor typo corrected and game structure set up

--- game.py
+++ game.py
@@ -1,10 +1,20 @@
 from PyQt6.QtWidgets import QApplication, QGraphicsScene, QGraphicsView
+from tower import Tower
 
 
 class Game(QGraphicsView):
     def __init__(self):
         super().__init__()
 
-        scene = QGraphicsScene()
+        # create a scene
+        self.gamescene = QGraphicsScene(self)
 
+        # set the scene
+        self.setScene(self.gamescene)
+
+        # create a tower
+        tower = Tower()
+
+        # add tower to the scene
+        self.gamescene.addItem(tower)
 
--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from PyQt6.QtWidgets import QApplication
+from game import Game
 import sys
+
+
+app = QApplication(sys.argv)
+
+game = Game()
+game.show()
+
+app.exec()
 
