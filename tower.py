a5d19678b177dc539adf6a92c3bec83b375bb5e4
Marco Auer <auer.marco007@gmail.com>
2026-01-17 16:13:01 +0100

    tower shooting with tower oriented coords, possible solution in comments

--- tower.py
+++ tower.py
@@ -1,6 +1,6 @@
 from PyQt6.QtCore import Qt, QObject, QPointF, QLineF, QTimer
 from PyQt6.QtWidgets import QGraphicsPixmapItem, QGraphicsPolygonItem
-from PyQt6.QtGui import QPixmap, QPolygonF, QColor
+from PyQt6.QtGui import QPixmap, QPolygonF
 from bullet import Bullet
 import res
 
@@ -8,8 +8,8 @@ class Tower(QGraphicsPixmapItem, QObject):
     def __init__(self, parent = None):
         super().__init__(parent)
 
-        self.setPixmap(QPixmap(":/images/images/provisional_tower.png").scaled(80, 80, Qt.AspectRatioMode.KeepAspectRatio))
-        self.attack_dest = (QPointF(100, 100))
+        self.setPixmap(QPixmap(":/images/images/provisional_tower_maybe_not_centered.png").scaled(80, 80, Qt.AspectRatioMode.KeepAspectRatio))
+        self.attack_dest = QPointF()
         self.timer = QTimer()
 
         # create a polygon from these points
@@ -38,8 +38,10 @@ class Tower(QGraphicsPixmapItem, QObject):
 
     def attackTarget(self):
         bullet = Bullet(self)
-        bullet.setPos(self.findXYCenter())
-
+        bullet.setPos(self.findXYCenter().x() - bullet.pixmap().width() / 2, self.findXYCenter().y() - bullet.pixmap().height() / 2)
+        
+        """ TODO As soon as enemy within game possible solution with coords -> game as towers parent, therefore tower should be able to acces enemys position(probably created by game) and
+        then set self.attack_dest = self.mapToScene(game.enemy)"""
         line = QLineF(self.findXYCenter(), self.attack_dest)
         angle = -1 * line.angle()
 
