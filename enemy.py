22b133673b26cdd90f34a7ca3a3b12422cf1920e
Marco Auer <auer.marco007@gmail.com>
2026-01-17 19:49:56 +0100

    enemy pre defined route visualized, multiple enemies spawnable, bug: tower shooting at multiple targets

--- enemy.py
+++ enemy.py
@@ -4,14 +4,14 @@ from PyQt6.QtGui import QPixmap
 import math
 
 class Enemy(QGraphicsPixmapItem, QObject): # wrong order endes in weird bug, why does QGraphicsPixmapItem have to be first?
-    def __init__(self, parent = None):
+    def __init__(self, points_to_follow,  parent = None):
         super().__init__(parent)
 
         # set graphics
         self.setPixmap(QPixmap(":/images/images/enemy.png").scaled(50, 50, Qt.AspectRatioMode.KeepAspectRatio))
 
         # set points
-        self.points = [QPointF(200, 200), QPointF(300, 300), QPointF(600, 300)]
+        self.points = points_to_follow
         self.point_index = 0
         self.dest = self.points[self.point_index]
 
@@ -23,15 +23,16 @@ class Enemy(QGraphicsPixmapItem, QObject): # wrong order endes in weird bug, why
         
     # move enemy forward at current angle
     def move_forward(self):
-        if self.dest == self.points[2]:
-            return
         # if close enough to dest, go to next dest
-        line = QLineF(self.pos(), self.dest)
-        if line.length() < 5 and len(self.points) - 1 > self.point_index:
+        line = QLineF(self.pos(), QPointF(self.dest.x() - self.boundingRect().width() / 2, self.dest.y() - self.boundingRect().height() / 2))
+        if line.length() < 5:
             self.point_index += 1
-            self.dest = self.points[self.point_index]
+            if self.point_index < len(self.points):
+                self.dest = self.points[self.point_index]
+            else:
+                self.timer.disconnect()
 
-        step_size = 2
+        step_size = 5
         # angle = self.rotation() # degrees # move enemy forward at current angle
         angle = -1 * line.angle()
         radians = math.radians(angle)
--- game.py
+++ game.py
@@ -1,6 +1,6 @@
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QGraphicsScene, QGraphicsView, QGraphicsPixmapItem
-from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt, QTimer, QPointF, QLineF
+from PyQt6.QtWidgets import QGraphicsScene, QGraphicsView, QGraphicsPixmapItem, QGraphicsLineItem
+from PyQt6.QtGui import QPixmap, QPen
 from tower import Tower
 from enemy import Enemy
 from build_basic_tower_icon import BuildBasicTowerIcon
@@ -38,14 +38,61 @@ class Game(QGraphicsView):
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
 
         # create enemy
-        enemy = Enemy()
-        self.gamescene.addItem(enemy)
+        self.timer = QTimer()
+        self.enemies_spawned = None
+        self.max_number_of_enemies = None
+        self.points_to_follow = [QPointF(800, 0), QPointF(450, 450), QPointF(0, 300)]
+        self.createEnemies(5)
+
+        # create road
+        self.createRoad()
 
         # test code
         """ tower = Tower()
         tower.setPos(210, 410)
         self.gamescene.addItem(tower) """
 
+    def setCursor(self, filename):
+        if self.cursor:
+            self.gamescene.removeItem(self.cursor)
+            del self.cursor
+
+        self.cursor = QGraphicsPixmapItem()
+        self.cursor.setPixmap(QPixmap(filename).scaled(80, 80, Qt.AspectRatioMode.KeepAspectRatio))
+        self.gamescene.addItem(self.cursor)
+
+    def createEnemies(self, number_of_enemies):
+        self.enemies_spawned = 0
+        self.max_number_of_enemies = number_of_enemies
+        self.timer.timeout.connect(self.createEnemy)
+        self.timer.start(1500)
+
+    def createEnemy(self):
+        # spawn an enemy
+        enemy = Enemy(self.points_to_follow)
+        enemy.setPos(self.points_to_follow[0].x() - enemy.boundingRect().width() / 2, self.points_to_follow[0].y() - enemy.boundingRect().height() / 2)
+        self.gamescene.addItem(enemy)
+        self.enemies_spawned += 1
+        if self.enemies_spawned >= self.max_number_of_enemies:
+            self.timer.disconnect()
+
+    def createRoad(self):
+        i = 0
+        for point in self.points_to_follow:
+            # create a line connecting the two points
+            if i < len(self.points_to_follow) - 1:
+                i += 1
+                line = QLineF(point, self.points_to_follow[i])
+                line_item = QGraphicsLineItem(line)
+
+                pen = QPen()
+                pen.setWidth(2)
+                pen.setColor(Qt.GlobalColor.red)
+
+                line_item.setPen(pen)
+                self.gamescene.addItem(line_item)
+
+    # events
     def mousePressEvent(self, event):
         if not self.build == None:
             for item in self.cursor.collidingItems():
@@ -59,15 +106,6 @@ class Game(QGraphicsView):
 
         else:
             super().mousePressEvent(event)
-
-    def setCursor(self, filename):
-        if self.cursor:
-            self.gamescene.removeItem(self.cursor)
-            del self.cursor
-
-        self.cursor = QGraphicsPixmapItem()
-        self.cursor.setPixmap(QPixmap(filename).scaled(80, 80, Qt.AspectRatioMode.KeepAspectRatio))
-        self.gamescene.addItem(self.cursor)
 
     def mouseMoveEvent(self, event):
         if self.cursor:
