a74d8478a1769ebcc52797ae6f88520e4b9ac2ee
Marco Auer <auer.marco007@gmail.com>
2026-01-17 16:13:01 +0100

    quick save

---
+++ enemy.py
@@ -0,0 +1,29 @@
+from PyQt6.QtCore import Qt, QObject, QPointF
+from PyQt6.QtWidgets import QGraphicsPixmapItem
+from PyQt6.QtGui import QPixmap
+
+class Enemy(QObject, QGraphicsPixmapItem):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+
+        # set graphics
+        self.setPixmap(QPixmap(":/images/images/enemy_cut_out.png"))
+
+        # set points
+        self.points = []
+        self.points.append(self.mapToScene(self.setPointPos(QPointF(), 50, 50)))
+        self.points.append(self.mapToScene(self.setPointPos(QPointF(), 100, 70)))
+        self.dest = QPointF()
+        self.point_index = None
+
+    def move_forward(self):
+        pass
+
+    def rotateToPoint(self, point):
+        pass
+
+    def setPointPos(self, point, x, y):
+        point.setX(x)
+        point.setY(y)
+
+        return point
